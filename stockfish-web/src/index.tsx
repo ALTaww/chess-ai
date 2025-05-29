28c18e54682aaf61d16753aa0c516a2429f03bc0
ALTaww <123657665+ALTaww@users.noreply.github.com>
2025-05-29 20:50:57 +0300

    add basename to BrowserRouter

--- stockfish-web/src/index.tsx
+++ stockfish-web/src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="chess-ai">
       <ErrorBoundary>
         <App />
       </ErrorBoundary>
