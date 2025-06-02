1a0cfa00cafeeef6a59013832179e227ef3d1e50
ALTaww <123657665+ALTaww@users.noreply.github.com>
2025-06-02 18:05:19 +0300

    add delete function for easier fen  paste

--- stockfish-web/src/shared/ui/FenInput/FenInput.tsx
+++ stockfish-web/src/shared/ui/FenInput/FenInput.tsx
@@ -1,9 +1,10 @@
 import { classNames } from "shared/lib/classNames/classNames";
 import cls from "./FenInput.module.scss";
 import { Input } from "../Input/Input";
-import { ChangeEvent } from "react";
-import { Button, ButtonSize, ButtonTheme } from "../Button/Button";
+import { ChangeEvent, useState } from "react";
+import { Button, ButtonTheme } from "../Button/Button";
 import copyImg from "shared/assets/copy.png";
+import krestImg from "shared/assets/krest.png";
 
 interface FenInputProps {
   className?: string;
@@ -16,9 +17,14 @@ export const FenInput = ({
   fenPosition,
   onChange,
 }: FenInputProps) => {
+  const [isNull, setIsNull] = useState(false);
+
   const handleFenInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const fen = e.target.value;
+    if (fen === "") return;
+    setIsNull(false);
     if (onChange) {
-      onChange(e.target.value);
+      onChange(fen);
     }
   };
 
@@ -27,13 +33,20 @@ export const FenInput = ({
     console.log("copied");
   };
 
+  const deleteFen = () => {
+    setIsNull(true);
+  };
+
   return (
     <div className={classNames(cls.FenInput, {}, [className])}>
       <Input
-        value={fenPosition}
+        value={isNull ? "" : fenPosition}
         onChange={handleFenInputChange}
         placeholder="Вставьте позицию FEN, чтобы начать редактирование"
       />
+      <Button onClick={deleteFen} theme={ButtonTheme.CLEAR} square>
+        <img src={krestImg} width={32} alt="Удалить" />
+      </Button>
       <Button
         onClick={copy}
         theme={ButtonTheme.CLEAR}
